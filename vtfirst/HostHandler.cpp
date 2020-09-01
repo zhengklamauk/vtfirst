1ab4f73749a2bddfef7be17d178b602ffae48551
zhengklamauk <187350730@qq.com>
2020-09-01 18:21:57 +0800

    7th commit form 2 succeed

--- vtfirst/HostHandler.cpp
+++ vtfirst/HostHandler.cpp
@@ -17,7 +17,7 @@ static void _VMMEntryPointEbd()
     g_GuestRegs.esp_ = _vmread(GUEST_RSP);
     g_GuestRegs.eip_ = _vmread(GUEST_RIP);
 
-    //KdPrint(("dw32ExitReason=%X\ng_Guest.Regs.esp_=%X\ng_Guest.Regs.eip_=%X\n", dw32ExitReason, g_GuestRegs.esp_, g_GuestRegs.eip_));
+    KdPrint(("dw32ExitReason=%X\ng_Guest.Regs.esp_=%X\ng_Guest.Regs.eip_=%X\n", dw32ExitReason, g_GuestRegs.esp_, g_GuestRegs.eip_));
 
     __asm int 3
 
